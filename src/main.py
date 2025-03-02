2b03a18ad89b4c1fe5aae08d3c1d734e34ccc899
Andrew Cho <andrewcho7@berkeley.edu>
2025-03-01 22:41:55 -0800

    fix import

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src import query_pinecone, format_context, generate_answer
+from core import query_pinecone, format_context, generate_answer
 
 st.title("UFC Chatbot")
 
